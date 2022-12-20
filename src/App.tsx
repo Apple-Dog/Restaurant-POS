8ac4de87e372e693afafbc3728422864da9fc88e
Anirban Middya <anirbanmiddya0011@gmail.com>
2022-12-20 14:47:36 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+
+   return (
+      <div className="App">
+         <h1 className="text-3xl font-bold">Welcome to POS!</h1>
+      </div>
+   );
+}
+
+export default App;
